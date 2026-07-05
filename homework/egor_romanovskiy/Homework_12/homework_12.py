310960a700c515c8cd1849e2940fd592fa272757
Egor Romanovskiy <egor.romanovskiy@ftco.ltd>
2026-07-05 14:52:53 +0300

    Homework_12 fixes

--- homework/egor_romanovskiy/Homework_12/homework_12.py
+++ homework/egor_romanovskiy/Homework_12/homework_12.py
@@ -1,26 +1,22 @@
 class Flower:
-    def __init__(self, name, price, length, color, quantity, lifetime):
+    def __init__(self, name, price, length, color, lifetime):
         self.name = name
         self.price = price
         self.length = length
         self.color = color
-        self.quantity = quantity
         self.lifetime = lifetime
 
     def get_info(self):
         print(f'Название: {self.name}, цена: {self.price} руб, длина: {self.length} см, цвет: {self.color}, '
-              f'количество: {self.quantity} шт, время жизни: {self.lifetime} дней')
+              f'время жизни: {self.lifetime} дней')
 
     def set_price(self, price):
         self.price = price
 
-    def reduce_quantity_by(self, number):
-        self.quantity = self.quantity - number
-
 
 class Rose(Flower):
-    def __init__(self, name, price, length, color, quantity, lifetime, has_thorns, variety):
-        super().__init__(name, price, length, color, quantity, lifetime)
+    def __init__(self, name, price, length, color, lifetime, has_thorns, variety):
+        super().__init__(name, price, length, color, lifetime)
         self.has_thorns = has_thorns
         self.variety = variety
 
@@ -31,8 +27,8 @@ class Rose(Flower):
 
 
 class Tulip(Flower):
-    def __init__(self, name, price, length, color, quantity, lifetime, texture, has_fringe):
-        super().__init__(name, price, length, color, quantity, lifetime)
+    def __init__(self, name, price, length, color, lifetime, texture, has_fringe):
+        super().__init__(name, price, length, color, lifetime)
         self.texture = texture
         self.has_fringe = has_fringe
 
@@ -43,8 +39,8 @@ class Tulip(Flower):
 
 
 class Lily(Flower):
-    def __init__(self, name, price, length, color, quantity, lifetime, number_of_petals):
-        super().__init__(name, price, length, color, quantity, lifetime)
+    def __init__(self, name, price, length, color, lifetime, number_of_petals):
+        super().__init__(name, price, length, color, lifetime)
         self.number_of_petals = number_of_petals
 
     def get_info(self):
@@ -67,12 +63,10 @@ class Bouquet:
         return price
 
     def avg_lifetime(self):
-        count = 0
         lifetime = 0
         for flower in self.flowers:
-            count += 1
             lifetime += flower.lifetime
-        return lifetime / count
+        return lifetime / len(self.flowers)
 
     def sort_by_price(self):
         self.flowers.sort(key=lambda flower: flower.price)
@@ -89,23 +83,23 @@ class Bouquet:
     def find_flower_by_color(self, color):
         for flower in self.flowers:
             if flower.color == color:
-                print(flower.name)
+                return flower
 
 
 red_rose = Rose(
-    'Красная Кристина', 30, 80, 'Бордовый', 50, 10,
+    'Красная Кристина', 30, 80, 'Бордовый', 10,
     True, 'Кусторвая'
 )
 juliet = Rose(
-    'Джульетта', 15, 80, 'Персиковый', 50, 10,
+    'Джульетта', 15, 80, 'Персиковый', 10,
     True, 'Кусторвая'
 )
 peach_tulip = Tulip(
-    'Пионовидный', 5, 45, 'Розовый', 123, 6,
+    'Пионовидный', 5, 45, 'Розовый', 6,
     'Гладкий', False
 )
-stargazer = Lily('Stargazer', 15, 30, 'Белый', 20, 20, 3)
-lollipop = Lily('Lollipop', 15, 30, 'Розовый', 20, 20, 3)
+stargazer = Lily('Stargazer', 15, 30, 'Белый', 20, 3)
+lollipop = Lily('Lollipop', 15, 30, 'Розовый', 20, 3)
 
 red_rose.get_info()
 juliet.get_info()
@@ -113,4 +107,4 @@ peach_tulip.get_info()
 stargazer.get_info()
 lollipop.get_info()
 
-birthday_bouquet = Bouquet([red_rose, juliet, stargazer])
+birthday_bouquet = Bouquet([red_rose, red_rose, red_rose, juliet, stargazer])
